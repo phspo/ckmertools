f1885181daff54f441b399d955076c84c0d65717
Höckesfeld, Jan (jahoe112) <jan.hoeckesfeld@uni-duesseldorf.de>
2021-02-06 20:20:10 +0100

    return early?

--- src/main.cpp
+++ src/main.cpp
@@ -72,7 +72,7 @@ using namespace boost::program_options;
             std::map<std::string,double> unexpectedKmerLikelihoods;
 
 
-            ctpl::thread_pool p(vm.count("cores") ? vm["cores"].as<int>() : 1 );
+            //ctpl::thread_pool p(vm.count("cores") ? vm["cores"].as<int>() : 1 );
 
             //std::vector<std::future< probabilistic::CoverageBasedResult>> results(expectedCounts.size());
             std::vector<probabilistic::CoverageBasedResult> results(expectedCounts.size()); 
@@ -112,60 +112,11 @@ using namespace boost::program_options;
             if (vm.count("unexpected")){
                 parsing::writeDictionary(unexpectedKmerLikelihoods,vm["unexpected"].as<std::string>());
             }
+            // stop from deallocing kmer_wrap_ptr?
+            kmer_wrap_ptr.get()->iterset;
             BOOST_LOG_TRIVIAL(info) << "DONE IF \n";
-        }
-        else if (mode == 1) {
-            Json::Value sequenceProfiles = parsing::readDictionary(vm["profiles"].as<std::string>());
-            Json::Value observedCounts = parsing::readDictionary(vm["observed"].as<std::string>());
-            
-            auto observedCountsPointer = std::make_shared<Json::Value>(observedCounts);
-
-            std::map<std::string,double> likelihoods;
-
-            float baseErrorRate = vm["baseerror"].as<float>();
-            int k = vm["k"].as<int>();
-
-            //Pre-Calculate all Hamming-Distance probabilities
-            double* hdLikelihoods =  new double[k+1];
-            for (int i=0;i<k+1;i++){
-                double intactBasesProbability = pow((1-baseErrorRate),(k-i));
-                double alteredBasesProbability = pow(baseErrorRate,i);
-                hdLikelihoods[i] = intactBasesProbability * alteredBasesProbability;
-            }
-
-            ctpl::thread_pool p(vm.count("cores") ? vm["cores"].as<int>() : 1 );
-
-            std::vector<std::future< probabilistic::GenerativeResult>> results(sequenceProfiles.size());
-
-            int idx = 0;
-            //Distribute tasks
-            for(Json::Value::const_iterator spaType=sequenceProfiles.begin(); spaType!=sequenceProfiles.end(); ++spaType,++idx) {
-                //std::cout << "Calculating spa Type: " << spaType.key().asString()+ "\n";
-                if (spaType->getMemberNames().size() > 0){
-                    //probabilistic::GenerativeResult result = probabilistic::calculateLikelihoodGenerative(idx,observedCounts,*spaType,baseErrorRate,hdLikelihoods);
-                    results[idx] = p.push(probabilistic::calculateLikelihoodGenerative,observedCountsPointer,*spaType,baseErrorRate,hdLikelihoods);
-                }
-                else{
-                    BOOST_LOG_TRIVIAL(info) << "No expected k-mers found for spa-type: " << spaType.key().asString() << ", maybe the type is too small? \n";
-                }
-            }
-            idx = 0;
-            //Collect results
-            for(Json::Value::const_iterator spaType=sequenceProfiles.begin(); spaType!=sequenceProfiles.end(); ++spaType, ++idx) {
-                //std::cout << "Calculating spa Type: " << spaType.key().asString()+ "\n";
-                if (spaType->getMemberNames().size() > 0){
-                    probabilistic::GenerativeResult result = results[idx].get();
-                    likelihoods.insert(std::pair<std::string,long double>(spaType.key().asString(),result.likelihood));
-                    //std::cout << result.likelihood << "\n";
-                }
-                else{
-                    //Handled before
-                }
-            }
-
-            delete hdLikelihoods;
-
-            parsing::writeDictionary(likelihoods,vm["target"].as<std::string>());
+            // return early?
+            return 0;
         }
         else{
             BOOST_LOG_TRIVIAL(error) << "Illegal Mode ... \n";
@@ -173,6 +124,7 @@ using namespace boost::program_options;
     }
     catch (const error &ex)
     {
+        BOOST_LOG_TRIVIAL(info) << "ERROR DETECTED \n";
         BOOST_LOG_TRIVIAL(error) << ex.what() << '\n';
     }
     BOOST_LOG_TRIVIAL(info) << "FINISHED \n";
