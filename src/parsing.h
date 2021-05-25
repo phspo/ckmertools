f9bb001dc384bbd30a34670f287b4489841e3c63
Höckesfeld, Jan (jahoe112) <jan.hoeckesfeld@uni-duesseldorf.de>
2021-05-25 19:41:25 +0200

    fix prev commit

--- src/parsing.h
+++ src/parsing.h
@@ -14,7 +14,7 @@ namespace parsing {
     Json::Value readDictionary(const std::string &filePath);
     void writeDictionary(const std::map<std::string,double> &map,const std::string &filePath);
     std::map<std::string, std::map<std::string, int>> get_hammingdistances(std::string distances_file, std::string kmers_idx_file);
-    std::map<std::string, std::map<std::string, int>> parsing::get_hammingdistancesO(std::string distances_file, std::string kmers_idx_fileO, std::string kmers_idx_fileV);
+    std::map<std::string, std::map<std::string, int>> get_hammingdistancesO(std::string distances_file, std::string kmers_idx_fileO, std::string kmers_idx_fileV);
     std::unordered_set<std::string> get_V(Json::Value &expectedCounts);
     std::unordered_set<std::string> get_O(Json::Value &observedCounts);
 };
